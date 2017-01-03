99b243b5cd75b07c20498814a8c24c3b0d42367d
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-01-03 15:20:46 +0330

    NeedleMovement Script Added and Modified.

---
+++ PinTheCircle/Assets/Scripts/NeedleScripts/NeedleMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleMovement : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject NeedleBody;
+
+	private float SpeedY = 5f;
+	private bool CanFireNeedle;
+	private bool NeedleTouchedTheCircle;
+	private Rigidbody2D MyBody;
+
+
+	void Awake ()
+	{
+		Initialize ();
+	}
+
+	void Start ()
+	{
+		FireTheNeedle ();
+	}
+
+	void Initialize ()
+	{
+
+		MyBody = gameObject.GetComponent<Rigidbody2D>();
+
+	}
+
+	void FireTheNeedle ()
+	{
+
+		MyBody.isKinematic = false;
+		CanFireNeedle = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (CanFireNeedle == true) {
+
+			MyBody.velocity = new Vector2 (0, SpeedY);
+
+		}
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D target)
+	{
+
+		if (NeedleTouchedTheCircle == true) {
+
+			return;
+
+		}
+
+		if (target.tag == "Circle") {
+
+			Debug.Log ("Needle Touched the Circle");
+			CanFireNeedle = false;
+			MyBody.isKinematic = true;
+			NeedleTouchedTheCircle = true;
+
+		}
+
+	}
+
+}
